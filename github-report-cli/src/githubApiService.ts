4f21558eeb68e397ca46f71c355f7727cef4d5d7
avimehenwal <avi.mehanwal@gmail.com>
2020-05-16 21:45:23 +0200

    feat(object): :lipstick: Get user repos data
    why are they only 30? seems like partial data is returned

--- github-report-cli/src/githubApiService.ts
+++ github-report-cli/src/githubApiService.ts
@@ -4,15 +4,21 @@ import { User } from './User'
 export class githubApiService {
 
   async getUserInfo(userName: string) {
-    let baseURL = 'https://api.github.com/users/'
-    const response = await axios.get(baseURL + userName)
+    let baseURL = 'https://api.github.com/users/' + userName
+    const response = await axios.get(baseURL)
     let jsondata = response.data
     console.log(jsondata)
     let avi = new User(jsondata);
     console.log(avi);
   }
 
-  getRepos() {
-
+  async getRepos(userName: string) {
+    let URL = 'https://api.github.com/users/' + userName + '/repos'
+    const response = await axios.get(URL)
+    let jsondata = response.data
+    console.log(jsondata)
+    console.log(typeof jsondata)
+    console.log(typeof jsondata.length)
+    console.log(jsondata.length)
   }
 }
--- github-report-cli/src/index.ts
+++ github-report-cli/src/index.ts
@@ -3,7 +3,9 @@ import { JSONResponse } from './User';
 
 
 let github = new githubApiService();
-github.getUserInfo('avimehenwal');
+// github.getUserInfo('avimehenwal');
+github.getRepos('avimehenwal');
+
 // console.log(githubApiService);
 // console.log(JSONResponse);
 
