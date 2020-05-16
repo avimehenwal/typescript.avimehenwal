98f77a416a4f31070f7223fb0ded0d407ebd2878
avimehenwal <avi.mehanwal@gmail.com>
2020-05-16 23:06:11 +0200

    feat(arguments): :card_file_box: Accept user arguments and print in colors

--- github-report-cli/src/index.ts
+++ github-report-cli/src/index.ts
@@ -1,11 +1,20 @@
 import { githubApiService } from './githubApiService';
 import { JSONResponse } from './User';
+import * as _ from 'lodash'
+import chalk from 'chalk';
 
 
-let github = new githubApiService();
-// github.getUserInfo('avimehenwal');
-github.getRepos('avimehenwal');
+if (process.argv.length < 3) {
+  console.log(chalk.red.bold('Kindly provide userName as argument!'));
+} else {
+  let username = process.argv[2]
+  console.log(chalk.green.bold('Username: ', username));
 
-// console.log(githubApiService);
-// console.log(JSONResponse);
+  let github = new githubApiService();
+  github.getUserInfo(username);
+  // github.getRepos(username);
+
+  // console.log(githubApiService);
+  // console.log(JSONResponse);
+}
 
